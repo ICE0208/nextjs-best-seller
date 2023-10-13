64dee52f6e1a78ff428bc2941b70771b04bbc0d8
ICE0208 <doky3307@naver.com>
2023-10-14 01:04:07 +0900

    feat: Home 페이지 기본 틀 완성

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,40 @@
-export default function IndexPage() {
-  return <div>Home</div>;
+export default function IndexPage({ data }) {
+  return (
+    <>
+      <div className="container">
+        {data.map((item, index) => (
+          <div
+            className="item"
+            key={index}
+          >
+            {item.display_name} &rarr;
+          </div>
+        ))}
+      </div>
+      <style jsx>{`
+        .container {
+          display: flex;
+          flex-wrap: wrap;
+          .item {
+            white-space: nowrap;
+            padding: 18px 20px;
+            border: 2px solid black;
+            margin: 20px;
+            font-weight: bold;
+            box-shadow: -4px 6px 10px -4px rgba(0, 0, 0, 0.75);
+            box-sizing: border-box;
+            -webkit-box-shadow: --4px 6px 10px -4px rgba(0, 0, 0, 0.75);
+            -moz-box-shadow: --4px 6px 10px -4px rgba(0, 0, 0, 0.75);
+          }
+        }
+      `}</style>
+    </>
+  );
 }
+
+export const getServerSideProps = async (context) => {
+  const res = await fetch("https://books-api.nomadcoders.workers.dev/lists");
+  const json = await res.json();
+  console.log("loading");
+  return { props: { data: json.results ?? [] } };
+};
